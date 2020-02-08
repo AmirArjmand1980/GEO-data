a48d5a9579daec32b4eadf616588290fb5158f7c
intel <amir.arjmand1980@gmail.com>
2020-02-08 20:38:16 +0330

    Starting Inspection Activity  .inspث

---
+++ app/src/main/java/com/amirarjmand/pgd/Inspection.java
@@ -0,0 +1,40 @@
+package com.amirarjmand.pgd;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.amirarjmand.pgd.Inspection_Frags.ViewPagerAdapter;
+
+public class Inspection extends AppCompatActivity {
+
+     private Toolbar toolbar;
+     private ViewPager viewPager;
+     private ViewPagerAdapter adapter;
+     private TabLayout tabLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        noTitle noTitle=new noTitle(Inspection.this);
+        setContentView(R.layout.activity_inspection);
+
+        toolbar=findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        viewPager=findViewById(R.id.pager);
+        adapter=new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        tabLayout=findViewById(R.id.tableLayout);
+
+
+        tabLayout.setupWithViewPager(viewPager);
+        
+        //hi
+
+    }
+    }
+                                                      
